48f427c5722934f7c88af84e3c6c9a0688e8d850
HarshAhrodia <harshahrodia@gmail.com>
2026-04-25 15:41:40 +0530

    Add maths problem code in JS

---
+++ untitled folder 2/Maths.js
@@ -0,0 +1,21 @@
+// Function to solve quadratic equation: ax^2 + bx + c = 0
+function solveQuadratic(a, b, c) {
+    const discriminant = b * b - 4 * a * c;
+    
+    if (discriminant > 0) {
+        const root1 = (-b + Math.sqrt(discriminant)) / (2 * a);
+        const root2 = (-b - Math.sqrt(discriminant)) / (2 * a);
+        return [root1, root2];
+    } else if (discriminant === 0) {
+        const root = -b / (2 * a);
+        return [root];
+    } else {
+        const realPart = (-b / (2 * a)).toFixed(2);
+        const imagPart = (Math.sqrt(-discriminant) / (2 * a)).toFixed(2);
+        return [`${realPart} + ${imagPart}i`, `${realPart} - ${imagPart}i`];
+    }
+}
+
+console.log("Roots of x^2 - 5x + 6 = 0:", solveQuadratic(1, -5, 6));
+console.log("Roots of x^2 - 4x + 4 = 0:", solveQuadratic(1, -4, 4));
+console.log("Roots of x^2 + 4 = 0:", solveQuadratic(1, 0, 4));
